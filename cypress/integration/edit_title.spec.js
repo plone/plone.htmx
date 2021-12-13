70222ec0a710602ff9febeb7e7960215043d6fdf
Godefroid Chapelle <gotcha@bubblenet.be>
2021-12-13 14:15:36 +0100

    Problem: there are no tests
    Solution: setup some tests with cypress

---
+++ cypress/integration/edit_title.spec.js
@@ -0,0 +1,29 @@
+describe('HTMX view', () => {
+  beforeEach(() => {
+    cy.setCookie('__ac', 'NjE2NDZkNjk2ZTo2MTY0NmQ2OTZl')
+  })
+
+  it('Edit title and cancel', () => {
+    cy.visit('http://localhost:8080/Plone/news/htmx_view')
+    cy.contains('Edit title').click()
+    cy.get('input[name="title"]').should('have.focus')
+    cy.contains('Cancel').click()
+    cy.contains('Actualités')
+  })
+
+  it('Edit title and save', () => {
+    cy.visit('http://localhost:8080/Plone/news/htmx_view')
+    cy.contains('Edit title').click()
+    cy.get('input[name="title"]').should('have.focus')
+    cy.get('input[name="title"]').type('{selectall}Cypress')
+    cy.contains('Save').click()
+    cy.get('li.selected').contains('Cypress')
+    cy.get('#breadcrumbs-current').contains('Cypress')
+    cy.contains('Cypress')
+    cy.contains('Edit title').click()
+    cy.get('input[name="title"]').should('have.focus')
+    cy.get('input[name="title"]').type('{selectall}Actualités')
+    cy.contains('Save').click()
+  })
+})
+
